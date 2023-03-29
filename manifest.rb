3bd275360e7de52adf05a3f4f48d9a88cbc755b6
Philipp Schlesinger <info@philcomm.dev>
2023-03-29 17:07:26 +0200

    Add a bunch of stuff

--- manifest.rb
+++ manifest.rb
@@ -5,6 +5,10 @@ Staticz::Manifest.define do
     sass :main
   end
 
+  sub :js do
+    js :sound_controller
+  end
+
   sub :sounds do
     Dir["src/sounds/*.mp3"].each do |sound|
       file sound.sub("src/sounds/", "")
---
+++ src/js/sound_controller.js
@@ -0,0 +1,41 @@
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+
+Stimulus.register("sound", class extends Controller {
+  static targets = ["root", "sound"]
+
+  async play() {
+    if (current_audio) {
+      await this.fadeSoundOut()
+      current_audio.pause()
+      this.resetAudio()
+    }
+    document.querySelectorAll(".sounds-sound").forEach((e) => {
+      e.classList.remove("playing")
+    })
+    current_audio = this.soundTarget
+    current_audio.play()
+
+    this.rootTarget.classList.add("playing")
+  }
+
+  async stop() {
+    await this.fadeSoundOut()
+    current_audio.pause()
+    this.resetAudio()
+    this.rootTarget.classList.remove("playing")
+  }
+
+  async fadeSoundOut() {
+    while (current_audio.volume != 0) {
+      let new_volume = current_audio.volume - 0.05 < 0 ? 0 : current_audio.volume - 0.05
+      current_audio.volume = new_volume
+      await sleep(50)
+    }
+  }
+
+  resetAudio() {
+    current_audio.currentTime = 0
+    current_audio.volume = 1
+    current_audio = null
+  }
+})
