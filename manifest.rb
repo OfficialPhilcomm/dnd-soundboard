a9f2d826693215477adfbc2d3454494d163bb68e
Philipp Schlesinger <info@philcomm.dev>
2023-03-30 15:25:10 +0200

    Add favicons

--- manifest.rb
+++ manifest.rb
@@ -14,4 +14,13 @@ Staticz::Manifest.define do
       file sound.sub("src/sounds/", "")
     end
   end
+
+  file "favicon.ico"
+  sub :favicon do
+    file "favicon-16x16.png"
+    file "favicon-32x32.png"
+    file "apple-touch-icon.png"
+    file "android-chrome-192x192.png"
+    file "android-chrome-512x512.png"
+  end
 end
