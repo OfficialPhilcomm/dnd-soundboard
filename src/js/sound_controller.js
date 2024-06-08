1d025c9c75ad0a864ba6f7f587787347174c54f4
Philipp Schlesinger <info@philcomm.dev>
2024-06-08 10:19:45 +0200

    Add sound controller

--- src/js/sound_controller.js
+++ src/js/sound_controller.js
@@ -1,52 +1,57 @@
-import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js";
 
-Stimulus.register("sound", class extends Controller {
-  static targets = ["root", "sound"]
+Stimulus.register(
+  "sound",
+  class extends Controller {
+    static targets = ["root", "sound"];
 
-  handlePlay() {
-    this.rootTarget.classList.add("playing")
-  }
-
-  handlePause() {
-    this.rootTarget.classList.remove("playing")
-  }
-
-  async togglePlay() {
-    if (this.rootTarget.classList.contains("playing")) {
-      await this.stop()
-    } else {
-      await this.play()
-    }
-  }
-
-  async play() {
-    if (current_audio) {
-      await this.fadeSoundOut()
-      current_audio.pause()
-      this.resetAudio()
+    handlePlay() {
+      this.rootTarget.classList.add("playing");
     }
 
-    current_audio = this.soundTarget
-    current_audio.play()
-  }
+    handlePause() {
+      this.rootTarget.classList.remove("playing");
+    }
 
-  async stop() {
-    await this.fadeSoundOut()
-    current_audio.pause()
-    this.resetAudio()
-  }
+    async togglePlay() {
+      if (this.rootTarget.classList.contains("playing")) {
+        await this.stop();
+      } else {
+        await this.play();
+      }
+    }
 
-  async fadeSoundOut() {
-    while (current_audio.volume != 0) {
-      let new_volume = current_audio.volume - 0.05 < 0 ? 0 : current_audio.volume - 0.05
-      current_audio.volume = new_volume
-      await sleep(50)
+    async play() {
+      if (current_audio) {
+        await this.fadeSoundOut();
+        current_audio.pause();
+        this.resetAudio();
+      }
+
+      current_audio = this.soundTarget;
+      current_audio.play();
+    }
+
+    async stop() {
+      console.log("stopping audio");
+      await this.fadeSoundOut();
+      current_audio.pause();
+      this.resetAudio();
+    }
+
+    async fadeSoundOut() {
+      while (current_audio.volume != 0) {
+        let new_volume =
+          current_audio.volume - 0.05 < 0 ? 0 : current_audio.volume - 0.05;
+        current_audio.volume = new_volume;
+        await sleep(50);
+      }
+    }
+
+    resetAudio() {
+      current_audio.currentTime = 0;
+      current_audio.volume = 1;
+      current_audio = null;
     }
   }
-
-  resetAudio() {
-    current_audio.currentTime = 0
-    current_audio.volume = 1
-    current_audio = null
-  }
-})
+);
